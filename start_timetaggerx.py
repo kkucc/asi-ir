5d5268c378f567065d196773a4c34dba38aba59e
kkucc <19polusadmiral@gmail.com>
2026-04-16 16:33:00 +0300

    tametagger

--- start_timetaggerx.py
+++ start_timetaggerx.py
@@ -7,28 +7,27 @@ def main():
     tagger = None
     try:
         tagger = TimeTagger.createTimeTagger()
-        print(f"Conneccted: {tagger.getModel()}, S/N: {tagger.getSerial()}")
+        print(f"Connected: {tagger.getModel()}, S/N: {tagger.getSerial()}")
         
-        # уровень триггера на 0.5 Вольт?
-        tagger.setTriggerLevel(CHANNEL, 1.7)
-        print(f"{CHANNEL} 0.5?")
+        tagger.setTriggerLevel(CHANNEL, 1.7)        
         rate = TimeTagger.Countrate(tagger, channels=[CHANNEL])
         
-        print(f"\n Reading {CHANNEL}\n")
+        print(f"\nReading channel {CHANNEL} (Press Ctrl+C to stop)\n")
         print("-" * 50)
         
         while True:
             time.sleep(0.2) 
-            ounts_hz = rate.getData()[0]
+            counts_hz = rate.getData()[0] 
+            
             if counts_hz > 0:
-                print(f" {CHANNEL} | Частота: {counts_hz:.2f} Гц")
+                print(f"Channel {CHANNEL} | Frequency: {counts_hz:.2f} Hz      ")
             else:
-                print(f"{CHANNEL} | Частота: 0.00 Гц", end='\r')
+                print(f"Channel {CHANNEL} | Frequency: 0.00 Hz", end='\r')
 
     except KeyboardInterrupt:
-        print("\n\nkeynord interapt")
+        print("\n\nKeyboard interrupt")
     except Exception as e:
-        print(f"\n {e}")
+        print(f"\nError: {e}")
     finally:
         if tagger is not None:
             TimeTagger.freeTimeTagger(tagger)
@@ -36,15 +35,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-#     Conneccted: Time Tagger Ultra, S/N: 22300011M7
-# 5 0.5?
-
-#  Reading 5
-
-# --------------------------------------------------
-
-#  name 'counts_hz' is not defined
-# Done.
-# PS C:\Users\QE LAB\Documents\Visual Studio 2022\vscode> 
-
